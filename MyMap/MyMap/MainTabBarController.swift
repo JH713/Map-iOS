3072c79cc3697f72f4c91c9a87e60347bf165362
JH713 <strider13@snu.ac.kr>
2023-11-19 22:07:45 +0900

    Init: 프로젝트 생성

---
+++ MyMap/MyMap/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  MyMap
+//
+//  Created by 이지현 on 2023/11/19.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
---
+++ MyMap/MyMap/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  MyMap
+//
+//  Created by 이지현 on 2023/11/19.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
